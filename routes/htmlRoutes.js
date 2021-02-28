16a279f8efc6860fa34dccad594875efc42bdf5e
dave vebber <davevebber@gmail.com>
2021-02-27 17:53:13 -0800

    html routes fixed

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,8 @@
-let path = require("path");
-let router = require("express").Router();
+let path = require('path');
+let router = require('express').Router();
 
-router.get("/notes", function(request, response) {
-    response.sendFile(path.join(__dirname,"../public/notes.html"));
+router.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname,'../Develop/public/notes.html'));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const app = express();
 
-const apiRoutes = require('./routes/apiRoutes');
+// routes
 const htmlRoutes = require('./routes/htmlRoutes');
 
+// port
 let PORT = process.env.PORT || 3009;
 
+// express
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// express static
 app.use(express.static('public'));
 
+// routing
 app.use('/', htmlRoutes);
 
+// server listening
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
