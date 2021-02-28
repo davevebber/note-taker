29bc06597a534e7b60cb88629a80766ec83dabde
dave vebber <davevebber@gmail.com>
2021-02-27 16:33:59 -0800

    added PORT variable

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 
 let PORT = process.env.PORT || 3001;
 
+
+
+
 app.listen(PORT, () => {
-    console.log(`API server now on port ` + PORT);
+    console.log(`API server now on port ${PORT}!`);
   });
