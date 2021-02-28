6e48f62d00cdaa1e5d587316520d6631fca49db2
dave vebber <davevebber@gmail.com>
2021-02-27 18:37:47 -0800

    html routes set up

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,4 +5,8 @@ router.get('/notes', function(req, res) {
     res.sendFile(path.join(__dirname,'../Develop/public/notes.html'));
 });
 
+router.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname,'../Develop/public/index.html'));
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,24 @@
+// modules
 const express = require('express');
 const app = express();
 
 // routes
-const htmlRoutes = require('./routes/htmlRoutes');
+const htmlRoutes = require('./routes/htmlRoutes.js');
+// const apiRoutes = require('./routes/apiRoutes.js');
 
 // port
 let PORT = process.env.PORT || 3009;
 
-// express
+// sets express to handle data 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // express static
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // routing
 app.use('/', htmlRoutes);
+// app.use('/api', apiRoutes);
 
 // server listening
 app.listen(PORT, () => {
